d742f907799f685083a5fa067646f26ccbc978bc
Benoit Perroud <benoit@noisette.ch>
2015-06-18 20:38:54 +0200

    Usage, expecting one argument

--- src/main/java/ch/daplab/hive/func/TestMeToo.java
+++ src/main/java/ch/daplab/hive/func/TestMeToo.java
@@ -6,6 +6,10 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 
 /**
  * Created by bperroud on 18-Jun-15.
+ *
+ * ADD JAR /home/bperroud/udf-starter/target/udf-starter-1.0.0-SNAPSHOT.jar;
+ * CREATE TEMPORARY FUNCTION testMeToo AS 'ch.daplab.hive.func.TestMeToo';
+ * SELECT playerID, testMeToo(playerID) FROM batting_2011 LIMIT 10;
  */
 @Description(name = "testMeFunction", value = "_FUNC_(domain) - Test function don't do anything smart")
 public class TestMeToo extends UDF {
--- src/main/java/ch/daplab/pig/func/TestMe.java
+++ src/main/java/ch/daplab/pig/func/TestMe.java
@@ -11,6 +11,14 @@ import java.io.IOException;
 /**
  * Amazing UDF expecting two arguments, and
  *
+ * REGISTER /home/bperroud/udf-starter/target/udf-starter-1.0.0-SNAPSHOT.jar;
+ * DEFINE testMe ch.daplab.pig.func.TestMe();
+ * data = LOAD 'test_udf.txt' USING PigStorage() AS (d:chararray);
+ * data = FOREACH data GENERATE
+ *   d,
+ *   testMe(d) as dToUpper;
+ * DUMP data;
+ *
  */
 public class TestMe  extends EvalFunc<String> {
 
@@ -21,7 +29,7 @@ public class TestMe  extends EvalFunc<String> {
             return null;
         }
 
-        if (input.get(0) == null || input.get(1) == null) {
+        if (input.get(0) == null) {
             return null;
         }
         return (StringUtils.upperCase((String)input.get(0)));
